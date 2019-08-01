88f0d6f544d16afa4b27f72e552d51878aca52c5
JChesh <sfuchesh@hotmail.com>
2019-08-01 14:47:28 -0400

    started main function, added api

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,37 @@
 var topics = ["Elephants","Giraffes","Pandas","Tigers","Lions","Bears","Cheetahs","Snakes","Spiders","Sharks","Whales"]
 var currentTopic = "";
 var newGif;
+
+function mainButtons() {
+
+    $("#animalButtons").empty();
+
+    for (v in topics) {
+        var subjectButton = $("<button>");
+        subjectButton.text(topics[v]);
+        subjectButton.addClass("subjectButton btn btn-dark")
+        $("#animalButtons").append(subjectButton);
+    }
+
+    $(".subjectButton").on("click", function () {
+ 
+        $("#animalsDiv").empty();
+
+        currentTopic = $(this).text();
+
+        var url = "https://api.giphy.com/v1/gifs/search";
+        url += '?' + $.param({
+            'q': currentTopic,
+            'api_key': "noKAZyT0GueYq6ZCpyQkrZon6MPl6zcX"
+        });
+        $.ajax({
+            url: url,
+            method: "GET"
+        }).then(function(response){
+
+            var result = response.data;
+
+        });
+    })
+
+}
