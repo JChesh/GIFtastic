83268c917f0c336ca080c49c8049eeb13e4916b0
JChesh <sfuchesh@hotmail.com>
2019-08-01 14:15:47 -0400

    added some var and array

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,5 @@
 // Hello
+// Topics array
+var topics = ["Elephants","Giraffes","Pandas","Tigers","Lions","Bears","Cheetahs","Snakes","Spiders","Sharks","Whales"]
+var currentTopic = "";
+var newGif;
