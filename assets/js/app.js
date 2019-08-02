1b8e505420fd2db4f20094bf8e4ade29de466a77
JChesh <sfuchesh@hotmail.com>
2019-08-02 10:54:13 -0400

    Added comments

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,10 +4,13 @@ var topics = ["Elephants","Giraffes","Pandas","Tigers","Lions","Bears","Cheetahs
 var currentTopic = "";
 var newGif;
 
+//Creating main function including buttons for the topics array
 function mainButtons() {
 
+    //Empty's topic button
     $("#animalButtons").empty();
 
+    //For loop creating buttons
     for (v in topics) {
         var subjectButton = $("<button>");
         subjectButton.text(topics[v]);
@@ -21,6 +24,7 @@ function mainButtons() {
 
         currentTopic = $(this).text();
 
+        //ajax GET call, creates url based on api key
         var url = "https://api.giphy.com/v1/gifs/search";
         url += '?' + $.param({
             'q': currentTopic,
@@ -37,13 +41,15 @@ function mainButtons() {
 
                 var stillImageUrl = result[i].images.downsized_still.url;
 
+                //Create div for the card and adding a class
                 var animalDiv = $("<div>");
-                
                 animalDiv.addClass("col-sm-4")
 
+                //Div for card
                 var divCard = $("<div>")
                 divCard.addClass("card")
 
+                //Create IMG, append to card
                 var animalGif = $("<img>").attr("src", stillImageUrl);
                 animalGif.attr("data-still", result[i].images.original_still.url)
                 animalGif.attr("data-animate", result[i].images.original.url)
@@ -51,6 +57,7 @@ function mainButtons() {
                 animalGif.addClass("card-img-top");
                 divCard.append(animalGif);
 
+                //Card title, append to card
                 var animalName = $("<div>")
                 animalName.addClass("card-body bg-dark text-white")
                 var animalNameHeading = $("<h5>")
@@ -59,15 +66,18 @@ function mainButtons() {
                 animalName.append(animalNameHeading);
                 divCard.append(animalName);
 
+                //Information for card to add rating
                 var animalInfoList = $("<ul>");
                 animalInfoList.addClass("list-group", "list-group-flush");
 
+                //GIF Rating and append
                 var animalInfoRating = $("<li>");
                 animalInfoRating.addClass("list-group-item bg-dark text-white");
                 animalInfoRating.append("Rating: " + result[i].rating);
                 animalInfoList.append(animalInfoRating);
                 divCard.append(animalInfoList);
 
+                //Append animalDiv to divCard
                 animalDiv.append(divCard)
                 $("#animalsDiv").append(animalDiv);
             }
@@ -89,16 +99,22 @@ function mainButtons() {
     })
 }
 
+//mainButtons function
 mainButtons();
 
+//New topic button if not already in topics
 $("#newGif").on("click", function(){
 
     event.preventDefault();
-
+    //New topic from user
     newGif = $("#userTopic").val().trim();
+    //Clear form
     $("#form")[0].reset();
+    //Scans for topics in the array
     if (topics.indexOf(newGif) === -1) {
+        //If none, adds to topics array
         topics.push(newGif);
+        //Run again
         mainButtons();
     }
     
